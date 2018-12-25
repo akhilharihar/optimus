2d59ea95976500579b0c66009bf170c95c74970f
akhil <akhil01@live.com>
2018-12-25 09:09:35 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="optimus",
+    version="0.0.1",
+    author="Akhil Harihar",
+    author_email="akhil01@live.com",
+    description="Python port of jenssegers/optimus Id obfuscation",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
